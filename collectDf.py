359d5676adc53c291fe76f0947ae705e31ce7901
Grey Whittney <grey@gr3yw0lf.com>
2013-07-23 12:37:55 -0400

    Added types.db definition, used only one dispatch with multiple values

--- collectDf.py
+++ collectDf.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python
 # ----------------------------------------------
-# collectDf.py - Simple module to collect details of the output of df and send to collectd for processing
+# collectDf.py - Simple module to collect details of the output of df via collectd process
+#  collectd sends to carbon/graphite on different host
 #
 # ----------------------------------------------
 # In collectd.conf:
 #
-#  Import "collectDF"
+#  Import "collectDf"
 #  <Module collectDf>
-#    Verbose false
+#    Verbose False/True
 #  </Module>
+
+# types.db for df = total:GAUGE:0:U, used:GAUGE:0:U, pcntUsed:GAUGE:0:100
+#  may need to replace df in original types.db with df_old if using a custom type.db file
 
 import collectd
 import subprocess
 import re
 import os
 
-Verbose = false
+Verbose = False
 
 def read(data=None):
+	vl = collectd.Values(plugin='mountedDrives')
+	vl.host=os.uname()[1]
+
 	# obtain the output of df
 	output = subprocess.check_output("df")
 	# split out the lines
 	lines = output.splitlines()
 
-	# collectd requires:
-	# dispatch([type][, values][, plugin_instance][, type_instance][, plugin][, host][, time][, interval]) -> None.
-	vl = collectd.Values(plugin='mountedDrives')
-	vl.host=os.uname()[1]
+	#/dev/sda1      487727216  11126468 452186748   3% /
+	#/dev/sde1      240362656 180626476  47526380  80% /mnt/hd
+	#/dev/sda6      478169232 236302260 217931280  53% /home
+
+	# need to send 3 values, total, size, pcntUsed for each line
 
 	# ignoring the first (the header), parse
 	for line in lines[1:]:
@@ -34,39 +42,27 @@ def read(data=None):
 		# TODO: allow volumes from config
 		if filesystem not in ["udev",  "none", "tmpfs"]:
 			# strip out /dev/, replace / to _
-			fs = re.sub("/dev/","", filesystem)
+			fs = re.sub("dev/","", filesystem[1:])
+			if mountPoint == "/":
+				mountPoint = "/ROOT"
 			mount = re.sub("/","_",mountPoint)
-
-			vl.plugin_instance="{}{}".format(fs,mount)
-			vl.type='bytes'
-			vl.type_instance='total'
-			vl.values = [ size ]
-			if verbose:
-				collectd.info("Dispatch %s, %s bytes-total: %s" % (fs, mount, total) )
-			vl.dispatch()
-			
-			vl.type_instance='used'
-			vl.values = [ used ]
-			if verbose:
-				collectd.info("Dispatch %s, %s bytes-used: %s" % (fs, mount, used) )
-			vl.dispatch()
-
 			usePcnt = re.sub("%","", usePcnt)
-			vl.type='gauge'
-			vl.type_instance='pcntUsed'
-			vl.values = [ usePcnt ]
-			if verbose:
-				collectd.info("Dispatch %s, %s gauge-pcntUsed: %s" % (fs, mount, usePcnt) )
+
+			vl.type='df'
+			vl.type_instance="{}{}".format(fs,mount)
+			vl.values = [ size, used, usePcnt ]
+			if Verbose:
+				collectd.info("collectDf: Dispatch %s%s [ %s, %s, %s ]" % (fs, mount, size,used,usePcnt) )
 			vl.dispatch()
 
 def configure_callback(conf):
-	global verbose
+	global Verbose
 	for node in conf.children:
 		if node.key == 'Verbose':
-			verbose = bool(node.values[0])
+			Verbose = bool(node.values[0])
 
 collectd.register_read(read)
-if verbose:
+if Verbose:
 	collectd.info('Init of collectDF: Verbose = true')
 collectd.register_config(configure_callback)
 
